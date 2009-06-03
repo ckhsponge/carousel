390409543fdf02eda4f3d1b7f2366a82ac20bae1
ckhmini <ckhmini@CKH-iMac.local>
2009-06-03 16:54:07 -0700

    runner

--- script/runner_mt.sh
+++ script/runner_mt.sh
@@ -4,4 +4,4 @@ export RUBYLIB=/home/1727/data/rubygems/lib
 export GEM_HOME=/home/1727/data/rubygems/gems
 
 echo $1
-/home/1727/containers/rails/carousel/current/script/runner -e production $1
+/home/1727/containers/rails/carousel/current/script/runner -e production "$1"
