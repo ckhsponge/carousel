3a17d94fd686f0af46a9fa32023ae3b73f26e6be
ckhmini <ckhmini@CKH-iMac.local>
2009-06-03 16:41:04 -0700

    runner

--- script/runner_mt.sh
+++ script/runner_mt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-export PATH=$PATH:/home/44693/data/rubygems/bin:/home/44693/data/rubygems/gems/bin
-export RUBYLIB=/home/44693/data/rubygems/lib
-export GEM_HOME=/home/44693/data/rubygems/gems
+export PATH=$PATH:/home/1727/data/rubygems/bin:/home/1727/data/rubygems/gems/bin
+export RUBYLIB=/home/1727/data/rubygems/lib
+export GEM_HOME=/home/1727/data/rubygems/gems
 
 echo $1
 /home/1727/containers/rails/carousel/current/script/runner -e production $1
